6e8150bda7028f9df24ef44be139f2228fcadd23
Abdul Kader Jilani <akjilani691995@gmail.com>
2025-08-21 17:03:20 +0600

    Delete recipes directory

--- recipes/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import RecipeCategory, Recipe, RecipeRating, UserFavoriteRecipe
-admin.site.register(RecipeCategory)
-admin.site.register(Recipe)
-admin.site.register(RecipeRating)
-admin.site.register(UserFavoriteRecipe) 
