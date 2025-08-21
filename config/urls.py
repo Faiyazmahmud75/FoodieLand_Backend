a1f3032353662ca96a6241924bef04dbb288632b
AKJilani <akjilani691995@gmail.com>
2025-08-21 11:04:45 +0600

    confiration files re structured based on Team work of yesterday meeting

---
+++ config/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
+urlpatterns = [
+	path("", SpectacularSwaggerView.as_view(url_name="schema"), name="root"),
+	path("admin/", admin.site.urls),
+	path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+	path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="docs"),
+	path("api/auth/", include("users.urls")),
+	path("api/recipes/", include("recipes.urls")),
+	path("api/blogs/", include("blogs.urls")),
+	path("api/interactions/", include("interactions.urls")),
+] 
